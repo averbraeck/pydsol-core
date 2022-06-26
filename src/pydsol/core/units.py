bf4b3821445b8ed5b53feb103a9678489d48b5da
Alexander Verbraeck <a.verbraeck@gmail.com>
2022-06-26 18:50:10 +0200

    Major simplification in units module; added Dimensionless quantity.

--- src/pydsol/core/units.py
+++ src/pydsol/core/units.py
@@ -27,33 +27,20 @@ Q = TypeVar('Q')
 
 
 class Quantity(Generic[Q], ABC, float):
-    
-    __unitlist = ('rad', 'sr', 'kg', 'm', 's', 'A', 'K', 'mol', 'cd')
-
-    @classmethod
-    @abstractmethod
-    def baseunit(cls) -> str:
-        """
-        Return the baseunit for this quantity, preferably an SI unit.
+    """
+    Abstract class properties
+    -------------------------
+    _baseunit : dict
+        Defines the baseunit for this quantity, preferably an SI unit.
         The baseunit is coded as a string, and should be present in the 
         units dictionary with a conversion factor of 1.
-        """
-
-    @classmethod
-    @abstractmethod
-    def units(cls) -> dict[str, float]:
-        """
-        Return the available units for this quantity with a conversion 
+    _units: dict[str, float]:
+        Defines the available units for this quantity with a conversion 
         factor to the baseunit. The units are specified in a dictionary,
         mapping each string descriptor for the unit onto the conversion
         factor to the baseunit.
-        """
-        
-    @classmethod
-    @abstractmethod
-    def displayunits(cls) -> dict[str, str]:
-        """
-        Return a dictionary with a translation of the unit as it is
+    _displayunits: dict[str, str]:
+        Defines a dictionary with a translation of the unit as it is
         entered in the code and the way the unit will be displayed in the
         str(..) and repr(..) methods. An example is the 'micro' sugn, 
         which is entered as 'mu' in the units, but should be displayed as 
@@ -62,20 +49,24 @@ class Quantity(Generic[Q], ABC, float):
         {'mum', '\03BCm'}. Another examples is the Angstrom sign for length. 
         In case no transformations are necessary for the units in the 
         quantity, an empty dict {} can be returned.
-        """
-        
-    @classmethod
-    @abstractmethod
-    def sisig(cls) -> dict[str, int]:
-        """
-        Return a dictionary with the SI signature of the quantity. The 
+    _sisig: dict[str, int]:
+        Defines  dictionary with the SI signature of the quantity. The 
         symbols that can be used are '1', 'kg', 'm', 's', 'A', 'K', 'mol',
         and 'cd'. For force (Newton), the SI signature would be given as
         {'kg': 1, 'm': 1, 's': -2} equivalent to kg.m/s^2. In addition to
         the SI-units, we allow 'rad' (m/m) and 'sr' (m^2/m^2) as well for
-        reasons of clarity. 
-        """
-        
+        reasons of clarity.
+    _mul: dict[Quantity, Quantity]
+        Defines with which quantities the defined quantity can be multiplied,
+        and what the resulting quantity will be.
+    _div: dict[Quantity, Quantity]
+        Defines by which quantities the defined quantity can be divided,
+        and what the resulting quantity will be.
+    """
+    
+    __unitlist = ('rad', 'sr', 'kg', 'm', 's', 'A', 'K', 'mol', 'cd')
+    """ SI units in the order they will be displayed, plus rad and sr""" 
+
     @classmethod
     @abstractmethod
     def instantiate(cls, value, unit: str=None, **kwargs) -> Q:
@@ -95,11 +86,11 @@ class Quantity(Generic[Q], ABC, float):
             when the provided unit is not defined for this quantity
         """
         if unit == None:
-            unitmultiplier = cls.units()[cls.baseunit()]
+            unitmultiplier = cls._units[cls._baseunit]
         else:
-            if not unit in cls.units():
+            if not unit in cls._units:
                 raise ValueError(f"unit {unit} not defined")
-            unitmultiplier = cls.units()[unit]
+            unitmultiplier = cls._units[unit]
         basevalue = value * unitmultiplier
         return super().__new__(cls, basevalue, **kwargs)
     
@@ -116,7 +107,7 @@ class Quantity(Generic[Q], ABC, float):
             when the provided unit is not defined for this quantity
         """
         if unit == None:
-            self._unit = self.baseunit()
+            self._unit = self._baseunit
         else:
             self._unit = unit
     
@@ -126,7 +117,7 @@ class Quantity(Generic[Q], ABC, float):
         Return the display value of the quantity. So, for Length(14, 'cm')
         the displayvalue is 14. 
         """
-        return float(self) / self.units()[self._unit]
+        return float(self) / self._units[self._unit]
     
     @property
     def si(self) -> float:
@@ -162,9 +153,9 @@ class Quantity(Generic[Q], ABC, float):
         ValueError
             when newunit is not defined for this quantity
         """
-        if not newunit in self.units():
+        if not newunit in self._units:
             raise ValueError(f"unit {newunit} not defined")
-        ret = self.instantiate(self.si, self.baseunit())
+        ret = self.instantiate(self.si, self._baseunit)
         ret._unit = newunit
         return ret
     
@@ -174,7 +165,7 @@ class Quantity(Generic[Q], ABC, float):
         of the current quantity. So, if _val(80) is called on Area(10, "ha"),
         the returning value will be 
         """
-        q = self.instantiate(si, self.baseunit())
+        q = self.instantiate(si, self._baseunit)
         q._unit = self._unit
         return q
 
@@ -255,12 +246,18 @@ class Quantity(Generic[Q], ABC, float):
         Raises
         ------
         ValueError
-            when the multiplication factor is not a float or an int
+            when the multiplication factor is not a float or an int, and the
+            multiplication is not between quantities
         """
-        if not (other.__class__ == float or other.__class__ == int):
-            raise ValueError("* operator needs float or int")
-        return self._val(float(self) * other)
-
+        if other.__class__ == float or other.__class__ == int:
+            return self._val(float(self) * other)
+        if other.__class__ in self.__class__._mul:
+            newclass = self.__class__._mul[other.__class__]
+            return newclass.instantiate(float(self) * float(other),
+                                        newclass._baseunit)
+        raise ValueError("* operator not defined for %s * %s",
+                         self, other)
+        
     def __neg__(self):
         """
         Return a new quantity containing the negation of the value of the
@@ -303,7 +300,8 @@ class Quantity(Generic[Q], ABC, float):
         Raises
         ------
         ValueError
-            when the multiplication factor is not a float or an int
+            when the multiplication factor is not a float or an int, and the
+            multiplication is not between quantities
         """
         return self.__mul__(other)
     
@@ -355,13 +353,17 @@ class Quantity(Generic[Q], ABC, float):
         Raises
         ------
         ValueError
-            when the divisor is not a float or an int
+            when the divisor is not a float or an int, and the division
+            is not between two quantities
         """
-        if self.__class__ == other.__class__:
-            return float(self) / float(other)
-        if not (other.__class__ == float or other.__class__ == int):
-            raise ValueError("/ operator needs float or int")
-        return self._val(float(self) / other)
+        if other.__class__ == float or other.__class__ == int:
+            return self._val(float(self) / other)
+        if other.__class__ in self.__class__._div:
+            newclass = self.__class__._div[other.__class__]
+            return newclass.instantiate(float(self) / float(other),
+                                        newclass._baseunit)
+        raise ValueError("/ operator not defined for %s * %s",
+                         self, other)
 
     def __trunc__(self):
         """
@@ -397,7 +399,7 @@ class Quantity(Generic[Q], ABC, float):
         Return a string representation of the quantity, where the chosen unit 
         follows the value without a space.
         """
-        return str(self.displayvalue) + self.displayunits().get(self._unit,
+        return str(self.displayvalue) + self._displayunits.get(self._unit,
                 self._unit)
 
     def __repr__(self):
@@ -405,12 +407,12 @@ class Quantity(Generic[Q], ABC, float):
         Return a string representation of the quantity, where the chosen unit 
         follows the value without a space.
         """
-        return str(self.displayvalue) + self.displayunits().get(self._unit,
+        return str(self.displayvalue) + self._displayunits.get(self._unit,
                 self._unit)
 
     @classmethod
     def siunits(cls, div:bool=True, hat:str='', dot:str='') -> str:
-        return Quantity._siunits(cls.sisig(), div, hat, dot)
+        return Quantity._siunits(cls._sisig, div, hat, dot)
     
     @staticmethod
     def _siunits(sistr: dict[str, int], div:bool=True, hat:str='',
@@ -441,86 +443,46 @@ class Quantity(Generic[Q], ABC, float):
         if len(t) > 0:
             s += "/" + t
         return s
-
+    
 
 class Duration(Quantity['Duration']):
-    __baseunit = 's'
-    __units = {'s': 1, 'ms': 1E-3, 'mus': 1E-6, 'ns': 1E-9, 'min': 60,
+    _baseunit = 's'
+    _units = {'s': 1, 'ms': 1E-3, 'mus': 1E-6, 'ns': 1E-9, 'min': 60,
               'minute': 60, 'hr': 3600, 'hour': 3600, 'h': 3600,
               'day': 24 * 60 * 60, 'd': 24 * 60 * 60, 'w': 7.0 * 86400.0,
               'wk': 7.0 * 86400.0, 'week': 7.0 * 86400.0}
-    __displayunits = {'mus': '\u03BCs'}
-    __sisig = {'s': 1}
+    _displayunits = {'mus': '\u03BCs'}
+    _sisig = {'s': 1}
+    _mul = {}
+    _div = {}
 
-    @classmethod
-    def baseunit(cls) -> str:
-        return cls.__baseunit
-    
-    @classmethod
-    def units(cls) -> dict[str, float]:
-        return cls.__units
-    
-    @classmethod
-    def displayunits(cls) -> dict[str, str]:
-        return cls.__displayunits
-    
-    @classmethod
-    def sisig(cls) -> dict[str, int]:
-        return cls.__sisig
-    
     @classmethod
     def instantiate(cls, value, unit: str=None, **kwargs):
         return Duration(value, unit, **kwargs)
 
 
 class Length(Quantity['Length']):
-    __baseunit = 'm'
-    __units = {'m': 1, 'meter': 1, 'nm': 1E-9, 'mum': 1E-6, 'mm': 1E-3,
+    _baseunit = 'm'
+    _units = {'m': 1, 'meter': 1, 'nm': 1E-9, 'mum': 1E-6, 'mm': 1E-3,
                'cm': 0.01, 'dm':0.1, 'dam': 10, 'hm':100, 'km':1000,
                'Mm': 1E6, 'Gm': 1E9,
                'in': 0.0254, 'inch': 0.0254, '"': 0.0254,
                'ft': 0.3048, 'foot': 0.3048,
                'yd': 0.9144, 'yard': 0.9144,
                'mi': 1609.344, 'mile': 1609.344}
-    __displayunits = {'mum': '\u03BCm'}
-    __sisig = {'m': 1}
-
-    @classmethod
-    def baseunit(cls) -> str:
-        return cls.__baseunit
-    
-    @classmethod
-    def units(cls) -> dict[str, float]:
-        return cls.__units
-
-    @classmethod
-    def displayunits(cls) -> dict[str, str]:
-        return cls.__displayunits
-    
-    @classmethod
-    def sisig(cls) -> dict[str, int]:
-        return cls.__sisig
+    _displayunits = {'mum': '\u03BCm'}
+    _sisig = {'m': 1}
+    _mul = {}
+    _div = {}
 
     @classmethod
     def instantiate(cls, value, unit: str=None, **kwargs):
         return Length(value, unit, **kwargs)
     
-    def __mul__(self, other):
-        if isinstance(other, Length):
-            return Area(float(self) * float(other), "m^2")
-        else:
-            return super().__mul__(other)
-
-    def __truediv__(self, other):
-        if isinstance(other, Duration):
-            return Speed(float(self) / float(other), "m/s")
-        else:
-            return super().__truediv__(other)
-
 
 class Speed(Quantity['Speed']):
-    __baseunit = 'm/s'
-    __units = {'m/s': 1, 'mm/s': 1E-3, 'cm/s': 0.01, 'dm/s':0.1,
+    _baseunit = 'm/s'
+    _units = {'m/s': 1, 'mm/s': 1E-3, 'cm/s': 0.01, 'dm/s':0.1,
                'mum/s': 1E-6, 'nm/s': 1E-9, 'dam/s': 10,
                'hm/s': 100, 'km/s':1000, 'km/h': 1000.0 / 3600.0,
                'in/s': 0.0254, 'inch/second': 0.0254,
@@ -528,24 +490,10 @@ class Speed(Quantity['Speed']):
                'yd/s': 0.9144, 'yard/second': 0.9144,
                'mi/s': 1609.344, 'mile/second': 1609.344,
                'mi/h': 0.44704, 'mile/hour': 0.44704}
-    __displayunits = {'mum/s': '\u03BCm/s'}
-    __sisig = {'m': 1, 's':-1}
-
-    @classmethod
-    def baseunit(cls) -> str:
-        return cls.__baseunit
-    
-    @classmethod
-    def units(cls) -> dict[str, float]:
-        return cls.__units
-
-    @classmethod
-    def displayunits(cls) -> dict[str, str]:
-        return cls.__displayunits
-    
-    @classmethod
-    def sisig(cls) -> dict[str, int]:
-        return cls.__sisig
+    _displayunits = {'mum/s': '\u03BCm/s'}
+    _sisig = {'m': 1, 's':-1}
+    _mul = {}
+    _div = {}
 
     @classmethod
     def instantiate(cls, value, unit: str=None, **kwargs):
@@ -553,33 +501,48 @@ class Speed(Quantity['Speed']):
 
 
 class Area(Quantity):
-    __baseunit = 'm^2'
-    __units = {'m^2': 1, 'nm^2': 1E-18, 'mum^2': 1E-12, 'mm^2': 1E-6,
+    _baseunit = 'm^2'
+    _units = {'m^2': 1, 'nm^2': 1E-18, 'mum^2': 1E-12, 'mm^2': 1E-6,
                'cm^2': 1E-4, 'dm^2':0.01, 'dam^2': 100,
                'hm^2': 1E4, 'km^2': 1E6,
                'in^2': 0.0254 * 0.0254,
                'ft^2': 0.3048 * 0.3048,
                'yd^2': 0.9144 * 0.9144,
                'mi^2': 1609.344 * 1609.344}
-    __displayunits = {'mum^2': '\u03BCm^2'}
-    __sisig = {'m': 2}
-    
-    @classmethod
-    def baseunit(cls) -> str:
-        return cls.__baseunit
-    
-    @classmethod
-    def units(cls) -> dict[str, float]:
-        return cls.__units
-
-    @classmethod
-    def displayunits(cls) -> dict[str, str]:
-        return cls.__displayunits
-    
-    @classmethod
-    def sisig(cls) -> dict[str, int]:
-        return cls.__sisig
+    _displayunits = {'mum^2': '\u03BCm^2'}
+    _sisig = {'m': 2}
+    _mul = {}
+    _div = {}
 
     @classmethod
     def instantiate(cls, value, unit: str=None, **kwargs):
         return Area(value, unit, **kwargs)
+
+
+class Dimensionless(Quantity):
+    _baseunit = ''
+    _units = {'': 1}
+    _displayunits = {}
+    _sisig = {}
+    _mul = {}
+    _div = {}
+    
+    @classmethod
+    def instantiate(cls, value, unit: str=None, **kwargs):
+        return Dimensionless(value, unit, **kwargs)
+    
+# -----------------------------------------------------------------------------
+# Multiplication and division of units to create new units
+# -----------------------------------------------------------------------------
+
+
+Duration._div = {Duration: Dimensionless}
+
+Length._mul = {Length: Area}
+Length._div = {Length: Dimensionless, Duration: Speed}
+
+Speed._mul = {Duration: Length}
+Speed._div = {Speed: Dimensionless}
+
+# Area._mul = {Length: Volume}
+Area._div = {Area: Dimensionless, Length: Length}
